164206ad9c7e92ee5efdce6ffd6018829880bbe7
vshkn <vshkn1090@gmail.com>
2024-05-13 05:06:49 +0300

    front

---
+++ src/modules/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../App.css';
+
+const Banner = () => {
+  return (
+    <div>
+      <div class="intro">
+        <h1>Платформа для танцювальної спільноти: для обміну досвідом та спільного навчання</h1>
+        <p>Ласкаво просимо на наш сайт для танцюристів різних стилів! Знайдіть напарника, навчайтеся та станцюйте в нашій активній танцювальній спільноті.</p>
+        <div className='start_button'>
+        <button>Почати</button>
+        <button>Переглянути трекер</button>
+        </div>
+        
+    </div>
+    
+    </div>
+  );
+};
+export default Banner;
---
+++ src/modules/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+
+const Header = () => {
+  return (
+    <div>
+      <header>
+        <nav>
+            <ul>
+                <li><a href="#">Головна</a></li>
+                <li><a href="#">Події</a></li>
+                <li><a href="#">Календар</a></li>
+                <li><a href="#">Про нас</a></li>
+                <li><a href="#">Підтримка</a></li>
+                <li><a href="#">Контакти</a></li>
+            </ul>
+        </nav>
+    </header>
+    
+    </div>
+  );
+};
+export default Header;
